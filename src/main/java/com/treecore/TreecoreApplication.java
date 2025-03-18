fbca9bde4046f59b963bd1106861ec4a8ec2c49f
junhyeog <h-kun@naver.com>
2025-03-18 23:47:19 +0900

    feat: Spring Boot 애플리케이션 메인 클래스 추가

---
+++ src/main/java/com/treecore/TreecoreApplication.java
@@ -0,0 +1,16 @@
+package com.treecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Tree Framework Core Library 애플리케이션 클래스
+ * 이 클래스는 라이브러리를 독립적으로 실행할 때 사용됩니다.
+ */
+@SpringBootApplication
+public class TreecoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreecoreApplication.class, args);
+    }
+} 
